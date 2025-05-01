8954201501248b0a9903f5e3ac0ef7aaa1c36174
Krishnakumar R (KK) <65895020+kk-src@users.noreply.github.com>
2025-05-01 00:27:18 +0000

    Use hostname arrived from server, let exceptions bubble up from service as per best pratices of the project.

--- src/Services/Azure/Postgres/PostgresService.cs
+++ src/Services/Azure/Postgres/PostgresService.cs
@@ -21,228 +21,180 @@ public class PostgresService : IPostgresService
 
     public PostgresService()
     {
-        try
-        {
-            _tokenCredential = new DefaultAzureCredential();
-            _armClient = new ArmClient(_tokenCredential);
-        }
-        catch (Exception ex)
-        {
-            throw new InvalidOperationException("Failed to initialize PostgresService.", ex);
-        }
+        _tokenCredential = new DefaultAzureCredential();
+        _armClient = new ArmClient(_tokenCredential);
     }
 
     private async Task<string> GetAccessTokenAsync()
     {
-        try
+        if (_cachedAccessToken != null && DateTime.UtcNow < _tokenExpiryTime)
         {
-            if (_cachedAccessToken != null && DateTime.UtcNow < _tokenExpiryTime)
-            {
-                return _cachedAccessToken;
-            }
-
-            var tokenRequestContext = new TokenRequestContext(new[] { "https://ossrdbms-aad.database.windows.net/.default" });
-            var accessToken = await _tokenCredential.GetTokenAsync(tokenRequestContext, CancellationToken.None);
-
-            _cachedAccessToken = accessToken.Token;
-            _tokenExpiryTime = accessToken.ExpiresOn.UtcDateTime.AddSeconds(-60); // Subtract 60 seconds as a buffer.
-
             return _cachedAccessToken;
         }
-        catch (Exception ex)
+
+        var tokenRequestContext = new TokenRequestContext(new[] { "https://ossrdbms-aad.database.windows.net/.default" });
+        var accessToken = await _tokenCredential.GetTokenAsync(tokenRequestContext, CancellationToken.None);
+
+        _cachedAccessToken = accessToken.Token;
+        _tokenExpiryTime = accessToken.ExpiresOn.UtcDateTime.AddSeconds(-60); // Subtract 60 seconds as a buffer.
+
+        return _cachedAccessToken;
+    }
+
+    private static string NormalizeServerName(string server)
+    {
+        if (!server.Contains('.'))
         {
-            throw new InvalidOperationException("Failed to retrieve access token.", ex);
+            return server + ".postgres.database.azure.com";
         }
+        return server;
     }
 
     public async Task<List<string>> ListDatabasesAsync(string subscriptionId, string resourceGroup, string user, string server)
     {
-        try
-        {
-            var accessToken = await GetAccessTokenAsync();
+        var accessToken = await GetAccessTokenAsync();
 
-            var connectionString = $"Host={server};Database=postgres;Username={user};Password={accessToken};";
+        var host = NormalizeServerName(server);
+        var connectionString = $"Host={host};Database=postgres;Username={user};Password={accessToken};";
 
-            await using var connection = new NpgsqlConnection(connectionString);
-            await connection.OpenAsync();
-            var query = "SELECT datname FROM pg_database WHERE datistemplate = false;";
-            await using var command = new NpgsqlCommand(query, connection);
-            await using var reader = await command.ExecuteReaderAsync();
-            var dbs = new List<string>();
-            while (await reader.ReadAsync())
-            {
-                dbs.Add(reader.GetString(0));
-            }
-            return dbs;
-        }
-        catch (Exception ex)
+        await using var connection = new NpgsqlConnection(connectionString);
+        await connection.OpenAsync();
+        var query = "SELECT datname FROM pg_database WHERE datistemplate = false;";
+        await using var command = new NpgsqlCommand(query, connection);
+        await using var reader = await command.ExecuteReaderAsync();
+        var dbs = new List<string>();
+        while (await reader.ReadAsync())
         {
-            throw new InvalidOperationException("Failed to list databases.", ex);
+            dbs.Add(reader.GetString(0));
         }
+        return dbs;
     }
 
     public async Task<string> ExecuteQueryAsync(string subscriptionId, string resourceGroup, string user, string server, string database, string query)
     {
-        try
+        var accessToken = await GetAccessTokenAsync();
+
+        var host = NormalizeServerName(server);
+        var connectionString = $"Host={host};Database={database};Username={user};Password={accessToken};";
+        await using var connection = new NpgsqlConnection(connectionString);
+        await connection.OpenAsync();
+
+        await using var command = new NpgsqlCommand(query, connection);
+        await using var reader = await command.ExecuteReaderAsync();
+
+        var columnNames = Enumerable.Range(0, reader.FieldCount)
+                               .Select(reader.GetName)
+                               .ToArray();
+        var rows = new List<object?[]>();
+        while (await reader.ReadAsync())
         {
-            var accessToken = await GetAccessTokenAsync();
-
-            var connectionString = $"Host={server};Database={database};Username={user};Password={accessToken};";
-            await using var connection = new NpgsqlConnection(connectionString);
-            await connection.OpenAsync();
-
-            await using var command = new NpgsqlCommand(query, connection);
-            await using var reader = await command.ExecuteReaderAsync();
-
-            var columnNames = Enumerable.Range(0, reader.FieldCount)
-                                   .Select(reader.GetName)
-                                   .ToArray();
-            var rows = new List<object?[]>();
-            while (await reader.ReadAsync())
-            {
-                var row = Enumerable.Range(0, reader.FieldCount)
-                                    .Select(i => reader.IsDBNull(i) ? null : reader.GetValue(i))
-                                    .ToArray();
-                rows.Add(row);
-            }
-
-            return JsonSerializer.Serialize(new { columnNames, rows });
+            var row = Enumerable.Range(0, reader.FieldCount)
+                                .Select(i => reader.IsDBNull(i) ? null : reader.GetValue(i))
+                                .ToArray();
+            rows.Add(row);
         }
-        catch (Exception ex)
-        {
-            throw new InvalidOperationException("Failed to execute query.", ex);
-        }
+
+        return JsonSerializer.Serialize(new { columnNames, rows });
     }
 
     public async Task<List<string>> ListTablesAsync(string subscriptionId, string resourceGroup, string user, string server, string database)
     {
-        try
-        {
-            var accessToken = await GetAccessTokenAsync();
+        var accessToken = await GetAccessTokenAsync();
 
-            var connectionString = $"Host={server};Database={database};Username={user};Password={accessToken};";
-            await using var connection = new NpgsqlConnection(connectionString);
-            await connection.OpenAsync();
-            var query = "SELECT table_name FROM information_schema.tables WHERE table_schema = 'public';";
-            await using var command = new NpgsqlCommand(query, connection);
-            await using var reader = await command.ExecuteReaderAsync();
-            var tables = new List<string>();
-            while (await reader.ReadAsync())
-            {
-                tables.Add(reader.GetString(0));
-            }
-            return tables;
-        }
-        catch (Exception ex)
+        var host = NormalizeServerName(server);
+        var connectionString = $"Host={host};Database={database};Username={user};Password={accessToken};";
+        await using var connection = new NpgsqlConnection(connectionString);
+        await connection.OpenAsync();
+        var query = "SELECT table_name FROM information_schema.tables WHERE table_schema = 'public';";
+        await using var command = new NpgsqlCommand(query, connection);
+        await using var reader = await command.ExecuteReaderAsync();
+        var tables = new List<string>();
+        while (await reader.ReadAsync())
         {
-            throw new InvalidOperationException("Failed to list tables.", ex);
+            tables.Add(reader.GetString(0));
         }
+        return tables;
     }
 
     public async Task<List<string>> GetTableSchemaAsync(string subscriptionId, string resourceGroup, string user, string server, string database, string table)
     {
-        try
-        {
-            var accessToken = await GetAccessTokenAsync();
-            var connectionString = $"Host={server};Database={database};Username={user};Password={accessToken};";
+        var accessToken = await GetAccessTokenAsync();
 
-            await using var connection = new NpgsqlConnection(connectionString);
-            await connection.OpenAsync();
-            var query = $"SELECT column_name, data_type FROM information_schema.columns WHERE table_name = '{table}';";
-            await using var command = new NpgsqlCommand(query, connection);
-            await using var reader = await command.ExecuteReaderAsync();
-            var schema = new List<string>();
-            while (await reader.ReadAsync())
-            {
-                schema.Add($"{reader.GetString(0)}: {reader.GetString(1)}");
-            }
-            return schema;
-        }
-        catch (Exception ex)
+        var host = NormalizeServerName(server);
+        var connectionString = $"Host={host};Database={database};Username={user};Password={accessToken};";
+
+        await using var connection = new NpgsqlConnection(connectionString);
+        await connection.OpenAsync();
+        var query = $"SELECT column_name, data_type FROM information_schema.columns WHERE table_name = '{table}';";
+        await using var command = new NpgsqlCommand(query, connection);
+        await using var reader = await command.ExecuteReaderAsync();
+        var schema = new List<string>();
+        while (await reader.ReadAsync())
         {
-            throw new InvalidOperationException("Failed to get table schema.", ex);
+            schema.Add($"{reader.GetString(0)}: {reader.GetString(1)}");
         }
+        return schema;
     }
 
     public async Task<string> GetServerConfigAsync(string subscriptionId, string resourceGroup, string user, string server)
     {
-        try
-        {
-            ResourceIdentifier resourceGroupId = ResourceGroupResource.CreateResourceIdentifier(subscriptionId, resourceGroup);
-            var rg = _armClient.GetResourceGroupResource(resourceGroupId);
-            var pgServer = await rg.GetPostgreSqlFlexibleServerAsync(server);
-            var pgServerData = pgServer.Value.Data;
+        ResourceIdentifier resourceGroupId = ResourceGroupResource.CreateResourceIdentifier(subscriptionId, resourceGroup);
+        var rg = _armClient.GetResourceGroupResource(resourceGroupId);
+        var pgServer = await rg.GetPostgreSqlFlexibleServerAsync(server);
+        var pgServerData = pgServer.Value.Data;
 
-            var result = new
+        var result = new
+        {
+            server = new
             {
-                server = new
+                name = pgServerData.Name,
+                location = pgServerData.Location,
+                version = pgServerData.Version,
+                sku = pgServerData.Sku?.Name,
+                storage_profile = new
                 {
-                    name = pgServerData.Name,
-                    location = pgServerData.Location,
-                    version = pgServerData.Version,
-                    sku = pgServerData.Sku?.Name,
-                    storage_profile = new
-                    {
-                        storage_size_gb = pgServerData.Storage?.StorageSizeInGB,
-                        backup_retention_days = pgServerData.Backup?.BackupRetentionDays,
-                        geo_redundant_backup = pgServerData.Backup?.GeoRedundantBackup
-                    }
+                    storage_size_gb = pgServerData.Storage?.StorageSizeInGB,
+                    backup_retention_days = pgServerData.Backup?.BackupRetentionDays,
+                    geo_redundant_backup = pgServerData.Backup?.GeoRedundantBackup
                 }
-            };
+            }
+        };
 
-            return JsonSerializer.Serialize(result);
-        }
-        catch (Exception ex)
-        {
-            throw new InvalidOperationException("Failed to get server configuration.", ex);
-        }
+        return JsonSerializer.Serialize(result);
     }
 
     public async Task<string> GetServerParameterAsync(string subscriptionId, string resourceGroup, string user, string server, string param)
     {
-        try
+        ResourceIdentifier resourceGroupId = ResourceGroupResource.CreateResourceIdentifier(subscriptionId, resourceGroup);
+        var rg = _armClient.GetResourceGroupResource(resourceGroupId);
+
+        // Get the PostgreSQL Flexible Server
+        var pgServer = await rg.GetPostgreSqlFlexibleServerAsync(server);
+
+        // Get the configuration parameter asynchronously
+        var configResponse = await pgServer.Value.GetPostgreSqlFlexibleServerConfigurationAsync(param);
+
+        if (configResponse?.Value?.Data == null)
         {
-            ResourceIdentifier resourceGroupId = ResourceGroupResource.CreateResourceIdentifier(subscriptionId, resourceGroup);
-            var rg = _armClient.GetResourceGroupResource(resourceGroupId);
-
-            // Get the PostgreSQL Flexible Server
-            var pgServer = await rg.GetPostgreSqlFlexibleServerAsync(server);
-
-            // Get the configuration parameter asynchronously
-            var configResponse = await pgServer.Value.GetPostgreSqlFlexibleServerConfigurationAsync(param);
-
-            if (configResponse?.Value?.Data == null)
-            {
-                throw new ArgumentException($"Parameter '{param}' not found.");
-            }
-
-            // Return the param value
-            return configResponse.Value.Data.Value;
+            throw new ArgumentException($"Parameter '{param}' not found.");
         }
-        catch (Exception ex)
-        {
-            throw new InvalidOperationException("Failed to get server parameter.", ex);
-        }
+
+        // Return the param value
+        return configResponse.Value.Data.Value;
     }
 
     public async Task<List<string>> ListServersAsync(string subscriptionId, string resourceGroup, string user)
     {
-        try
-        {
-            ResourceIdentifier resourceGroupId = ResourceGroupResource.CreateResourceIdentifier(subscriptionId, resourceGroup);
-            var rg = _armClient.GetResourceGroupResource(resourceGroupId);
+        ResourceIdentifier resourceGroupId = ResourceGroupResource.CreateResourceIdentifier(subscriptionId, resourceGroup);
+        var rg = _armClient.GetResourceGroupResource(resourceGroupId);
 
-            var serverList = new List<string>();
+        var serverList = new List<string>();
 
-            await foreach (PostgreSqlFlexibleServerResource server in rg.GetPostgreSqlFlexibleServers().GetAllAsync())
-            {
-                serverList.Add(server.Data.Name);
-            }
-            return serverList;
-        }
-        catch (Exception ex)
+        await foreach (PostgreSqlFlexibleServerResource server in rg.GetPostgreSqlFlexibleServers().GetAllAsync())
         {
-            throw new InvalidOperationException("Failed to list servers.", ex);
+            serverList.Add(server.Data.Name);
         }
+        return serverList;
+
     }
 }
