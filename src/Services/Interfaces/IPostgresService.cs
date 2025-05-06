7096d3f940cf2f7622d903343e438d4a346f4ccf
Krishnakumar R (KK) <65895020+kk-src@users.noreply.github.com>
2025-05-06 08:50:16 +0000

    Remove the extra line

--- src/Services/Interfaces/IPostgresService.cs
+++ src/Services/Interfaces/IPostgresService.cs
@@ -16,5 +16,4 @@ public interface IPostgresService
     Task<List<string>> ListServersAsync(string subscriptionId, string resourceGroup, string user);
     Task<string> GetServerConfigAsync(string subscriptionId, string resourceGroup, string user, string server);
     Task<string> GetServerParameterAsync(string subscriptionId, string resourceGroup, string user, string server, string param);
-
 }
